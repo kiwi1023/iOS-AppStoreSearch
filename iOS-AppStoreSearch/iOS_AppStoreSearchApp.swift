f848fd9653f32017f7055c1118ebd1c6b06c4b98
Kiwi <toskw@naver.com>
2023-09-26 22:35:15 +0900

    add: 프로젝트 파일 생성 및 추가
    - gitignore 업데이트

---
+++ iOS-AppStoreSearch/iOS_AppStoreSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AppStoreSearchApp.swift
+//  iOS-AppStoreSearch
+//
+//  Created by Kiwon Song on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AppStoreSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
