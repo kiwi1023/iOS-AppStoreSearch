d981242dd8b12c2cb5587638f188c97e24ce5e4f
Kiwi <toskw@naver.com>
2023-09-29 16:24:45 +0900

    feature: SearchView 구현

---
+++ iOS-AppStoreSearch/Source/Presentation/SeachAppScene/AppDetailScene/AppDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppDetailView.swift
+//  iOS-AppStoreSearch
+//
+//  Created by Kiwon Song on 2023/09/29.
+//
+
+import SwiftUI
+
+struct AppDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppDetailView()
+    }
+}
--- iOS-AppStoreSearch/Source/Presentation/SeachAppScene/SearchAppView/SearchView.swift
+++ iOS-AppStoreSearch/Source/Presentation/SeachAppScene/SearchAppView/SearchView.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct SearchView: View {
+    @StateObject var viewModel = SearchViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            SearchStateView()
+                .environmentObject(viewModel)
+                .navigationTitle("검색")
+                .navigationDestination(for: AppInfo.self) { result in
+                  AppDetailView()
+                }
+        }
+        .searchable(
+            text:
+                Binding(
+                    get: { viewModel.searchText },
+                    set: { viewModel.action(.updateSuggestions($0))}
+                ),
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: "App Store"
+        )
+        .alert(isPresented: $viewModel.showingAlert) {
+            Alert(
+                title:
+                    Text("네트워크 오류"),
+                message: Text("인터넷 연결을 확인하세요."),
+                dismissButton: .default(
+                    Text("확인"),
+                    action: {
+                        viewModel.action(.search(viewModel.searchText))
+                    }
+                )
+            )
+        }
+        .onSubmit(of: .search) {
+            viewModel.action(.search(viewModel.searchText))
+        }
+        .onAppear {
+            viewModel.action(.onAppear)
+        }
+        .searchSuggestions {
+            if viewModel.searchState == .defualt {
+                suggestionView
+            }
+        }
+        .animation(.easeInOut, value: viewModel.searchState)
     }
-}
-
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView()
+    
+    private var suggestionView: some View {
+        ForEach(viewModel.suggestions, id: \.self) { suggestion in
+            HStack(spacing: 0) {
+                Image(systemName: "magnifyingglass")
+                    .padding(.trailing, 8)
+                Text(suggestion)
+            }
+            .onTapGesture {
+                viewModel.action(.search(suggestion))
+            }
+        }
+        .listStyle(.plain)
     }
 }
