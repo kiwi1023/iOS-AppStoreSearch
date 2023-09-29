148083ef38c4ae5f3f6b59fa46908d14b475656c
Kiwi <toskw@naver.com>
2023-09-29 17:01:25 +0900

    feature: ReleaseNoteView 구현

---
+++ iOS-AppStoreSearch/Source/Presentation/SeachAppScene/AppDetailScene/Body/ReleaseNoteView.swift
@@ -0,0 +1,41 @@
+//
+//  ReleaseNoteView.swift
+//  iOS-AppStoreSearch
+//
+//  Created by Kiwon Song on 2023/09/29.
+//
+
+import SwiftUI
+
+struct ReleaseNoteView: View {
+    var result: AppInfo
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            HStack(alignment: .firstTextBaseline) {
+                Text("새로운 기능")
+                    .font(.title2.bold())
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("버전 기록")
+                }
+            }
+            
+            HStack {
+                Text("버전 \(result.version)")
+                Spacer()
+                Text(result.currentReleaseDate.updateDatePassedDayText())
+            }
+            .font(.callout)
+            .foregroundColor(.secondary)
+        
+            ExpandableTextView(text: result.releaseNotes ?? "")
+            
+            Divider().padding(.vertical)
+        }
+        .padding(.horizontal, 16)
+    }
+}
